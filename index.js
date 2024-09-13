70610c4fdc191ebfb42de5a80bff57114d59f038
clarinha <clarinhag070@gmail.com>
2024-09-13 12:57:39 -0300

    adiciona metas aberas

--- index.js
+++ index.js
@@ -72,10 +72,31 @@ const metasRealizadas = async () =>
 
     await select 
     ({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
     })
    
+}
+
+const metasAbertas = async () => 
+
+{
+    const abertas = metas.filter ((meta) => 
+        {
+            return meta.checked != true
+        })
+
+        if(abertas.length == 0)
+        {
+            console.log ("Não existem metas abertas! :)")
+            return
+        }
+
+        await select 
+        ({
+            message: "Metas Abertas " + abertas.length,
+            choices: [...abertas]
+        })
 }
 
 const start = async () =>
@@ -100,6 +121,10 @@ const start = async () =>
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -121,6 +146,11 @@ const start = async () =>
             case "realizadas":
                 await metasRealizadas ()
                 break
+
+            case "abertas":
+                await metasAbertas ()
+                break
+
             case "sair":
                 console.log("Até a próxima!")
                 return
